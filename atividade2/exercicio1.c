c440a258e22387e52b1be7b97498733911a4ace9
MARIA CLARA RODRIGUES SILVA <mariaclara.rodrigues@a.ucb.br>
2023-08-14 10:53:56 -0300

    adicionando pasta atividades2

---
+++ atividade2/exercicio1.c
@@ -0,0 +1,7 @@
+/*
+    Exercício 1 - Soma dos elementos de um array. Escreva uma função que recebe um array 
+    de números inteiros e retorna a soma de todos os elementos;
+
+    Exemplo: soma_array([5, 7, 9,6])
+    Saída: 27 
+*/
