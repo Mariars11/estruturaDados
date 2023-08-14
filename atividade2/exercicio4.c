e1dfd3fd2ea7659a0c20576e2ca778a71d023bfc
Maria Clara <maria.clara@bcb.gov.br>
2023-08-14 14:24:24 -0300

    Resolucao exercicio4

--- atividade2/exercicio4.c
+++ atividade2/exercicio4.c
@@ -6,3 +6,24 @@
     Exemplo: conta_int_array([5, 7, 9, 6, 7, 11, 6, 5], 7)
     Saída: 2
 */
+
+#include <stdio.h>
+
+int ContadorNumero(int length, int* array, int busca){
+    int contador = 0;
+    
+    for(int i = 0; i < length; i++){
+        if(array[i] == busca){
+            contador++;
+        }
+    }
+    return contador;
+}
+
+int main(){
+    int array[] = {1,1,1,3,3,3,3,9,9,9,9,9}; // 1 (3), 3 (4), 9 (4)
+    int length = (sizeof(array) / sizeof(int));
+    
+    printf("%d", ContadorNumero(length, array, 9));
+    //esperado 5, 13
+}
