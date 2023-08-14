906f7b47d78c8a68a3071c349c7b3acff378b0d6
MARIA CLARA RODRIGUES SILVA <mariaclara.rodrigues@a.ucb.br>
2023-08-14 10:35:09 -0300

    Adicionando material da aula

---
+++ Material-Aula/14.08.23/aula.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    //declaração de string
+    char* string = "Testando uso de strings";
+    //tamanho da string
+    printf("%d", strlen(string));
+}
