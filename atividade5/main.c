698b1f0ac6303d07185ecfa4815a7a505464218d
Maria Clara <maria.clara@bcb.gov.br>
2023-09-11 16:22:43 -0300

    Criando arquivo palavras ordenadas

--- atividade5/main.c
+++ atividade5/main.c
@@ -3,34 +3,27 @@
 #include <string.h>
 #include "bubble_sort.h"
 
-void imprimirArray(char *array[], int length){
-    for(int i = 0; i < length; i++){
-        printf("%s ", array[i]);
-    }
-}
-
 int main(int argc, char* argv[]){
     //abrir o arquivo
     FILE *stream = fopen(argv[1], "r"); 
-    
+    //Tratativa caso o arquivo dê erro
     if(stream == NULL){
         perror("Não foi possivel abrir o arquivo");
         exit(1);
     }
-    //contador para o while
+    //quantidade de linhas para criação do array
     int qt_linhas = 0;
     //linhas a serem lidas  
     char line[101];
-    
-    // //lê as linhas
+    //contagem das linhas
     while (fgets(line, 101, stream)) 
     {
-        //incrementa
         qt_linhas++;
     }
+    //criação do array de palavras
     char *array[qt_linhas];
     int j = 0;
-    
+    //fechar o arquivo para poder lê-lo de novo
     fclose(stream);
     stream = fopen(argv[1], "r"); 
     
@@ -38,19 +31,29 @@ int main(int argc, char* argv[]){
         perror("Não foi possivel abrir o arquivo");
         exit(1);
     }
+    //aloca no array as palavras
     while (fgets(line, 101, stream)) 
     {
-        char* linha = _strdup(line);
-        array[j] = _strdup(linha);
+        array[j] = _strdup(line); //faz o malloc e o strcpy
         j++;
     }
+    fclose(stream);
 
     int length = sizeof(array) / sizeof(char*);
-    imprimirArray(array, length);
     bubble_sort(array, length);
-    printf("\n");
-    imprimirArray(array, length);
+    
+    FILE *streamSaida = fopen("arq_palavras_ordenado.txt", "w");
 
+    if(streamSaida == NULL){
+        perror("Não foi possivel abrir o arquivo");
+        exit(1);
+    }
+
+    for(int i = 0; i < length; i++){
+        fprintf(streamSaida, "%s\n", array[i]);
+    }
+
+    fclose(streamSaida);
     
     return 0;
 }
